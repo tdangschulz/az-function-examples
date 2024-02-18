c436a0ea15f60737e6fe68b481d33d419cd7e407
tuan <dev@dangschulz.de>
2024-02-18 13:34:38 +0100

    add bastion

--- loadbalancer/create-env.sh
+++ loadbalancer/create-env.sh
@@ -7,13 +7,26 @@ email=foo@bar.com
 vnet_name=vnet-lb-weu-tds
 vm_name=vm-lb-we-tds # max 14 digits
 subnet_name=subnet-lb-westeu-tds
+nat_gateway_name=nat-lb-wger-tds
 availability_set_name=as-lb-westeu-tds
 network_security_group=nsg-vm-lb-westeu-tds
 public_ip_name=pip-lb-tds
 
+bastion_name=bastion-lb-tds
+bastion_public_ip_name=pip-lb-bastion-tds
+bastion_subnet_name=AzureBastionSubnet
+
+
+
 az group create -g $resource_group -l $region
 
 az network public-ip create --name $public_ip_name --resource-group $resource_group
+az network public-ip create --name $bastion_public_ip_name --resource-group $resource_group
+
+az network nat gateway create --name $nat_gateway_name \
+                              --resource-group $resource_group \
+                              --location $region \
+                              --public-ip-addresses $public_ip_name
 
 az network vnet create \
   -n $vnet_name \
@@ -22,6 +35,18 @@ az network vnet create \
   --address-prefixes '10.0.0.0/16' \
   --subnet-name $subnet_name \
   --subnet-prefixes '10.0.1.0/24'
+
+az network vnet subnet create --name $bastion_subnet_name \
+                              --resource-group $resource_group \
+                              --vnet-name $vnet_name \
+                              --address-prefixes '10.0.2.0/24' \
+                              --nat-gateway $nat_gateway_name 
+                  
+az network bastion create --name $bastion_name \
+                          --public-ip-address $bastion_public_ip_name \
+                          --resource-group $resource_group \
+                          --location $region \
+                          --vnet-name $vnet_name
   
 az vm availability-set create \
   -n $availability_set_name \
