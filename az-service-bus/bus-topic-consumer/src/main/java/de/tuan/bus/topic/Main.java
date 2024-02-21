171438d775613746672356ef74186295820bb444
tuan <dev@dangschulz.de>
2024-02-21 22:02:42 +0100

    add examples

---
+++ az-service-bus/bus-topic-consumer/src/main/java/de/tuan/bus/topic/Main.java
@@ -0,0 +1,58 @@
+package de.tuan.bus.topic;
+
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusErrorContext;
+import com.azure.messaging.servicebus.ServiceBusProcessorClient;
+import com.azure.messaging.servicebus.ServiceBusReceivedMessage;
+import com.azure.messaging.servicebus.ServiceBusReceivedMessageContext;
+
+/**
+ * https://github.com/Azure/azure-sdk-for-java/blob/main/sdk/servicebus/azure-messaging-servicebus/README.md#send-messages
+ */
+public class Main {
+    private static final String CONNECTION_STRING = "xx";
+    private static final String TOPIC_NAME = "myfirsttopic";
+    private static final String SUBSCRIPTION_NAME = "mein-test-subscription";
+
+    public static void main(String[] args) {
+        ServiceBusProcessorClient processorClient = new ServiceBusClientBuilder()
+                .connectionString(CONNECTION_STRING)
+                .processor()
+                .topicName(TOPIC_NAME)
+                .subscriptionName(SUBSCRIPTION_NAME)
+                .processMessage(Main::processMessage)
+                .processError(context -> processError(context))
+                .buildProcessorClient();
+
+        System.out.println("Warte auf Nachrichten...");
+        processorClient.start();
+
+        waitForEnterKeyPress();
+
+        System.out.println("Beendet den Processor...");
+        processorClient.close();
+    }
+
+    private static void processMessage(ServiceBusReceivedMessageContext context) {
+        ServiceBusReceivedMessage message = context.getMessage();
+        System.out.printf("Empfangene Nachricht: '%s'%n", message.getBody());
+        // context.deadLetter(new
+        // DeadLetterOptions().setDeadLetterErrorDescription("Alles doof"));
+
+        // Weitere Verarbeitung hier
+    }
+
+    private static void processError(ServiceBusErrorContext exception) {
+        System.out.printf("Fehler beim Empfangen von Nachrichten: %s%n", exception.getException().getMessage());
+        // Fehlerbehandlung hier
+    }
+
+    private static void waitForEnterKeyPress() {
+        System.out.println("Drücken Sie 'Enter', um den Processor zu beenden.");
+        try {
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ az-service-bus/bus-topic-sender/src/main/java/de/tuan/bus/topic/Main.java
@@ -0,0 +1,37 @@
+package de.tuan.bus.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+
+/**
+ * https://github.com/Azure/azure-sdk-for-java/blob/main/sdk/servicebus/azure-messaging-servicebus/README.md#send-messages
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            var sender = new ServiceBusClientBuilder().connectionString(
+                    "xx")
+                    .sender().topicName("myfirsttopic")
+                    .buildClient();
+
+            var msg1 = new ServiceBusMessage("topic-msg-1");
+            msg1.getApplicationProperties().put("name", "tuan");
+            var msg2 = new ServiceBusMessage("topic-msg-2");
+
+            List<ServiceBusMessage> messages = Arrays.asList(
+                    msg1,
+                    msg2);
+
+            sender.sendMessages(messages);
+            sender.close();
+        } catch (Exception e) {
+            System.err.println("Ein Fehler ist aufgetreten: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+}
